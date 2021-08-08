ba90b2158ce2e7371d96c8f41d4c9407827be320
Cobatsu <huze.ozr@gmail.com>
2021-08-08 03:03:02 +0200

    Stopped tracking .env File

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,11 +26,11 @@ import { WebSocketLink } from '@apollo/client/link/ws';
 import { getMainDefinition } from '@apollo/client/utilities';
 
 const httpTerminatingLink = new HttpLink({
-  uri:"https://groupchattappp.herokuapp.com/graphql"
+  uri:"http://localhost:8000/graphql"
 });
 
 const wsLink = new WebSocketLink({
-  uri: "wss://groupchattappp.herokuapp.com/subscriptions",
+  uri: "wss://localhost:8000/subscriptions",
   options: {
     reconnect: true,
     connectionParams:{
