36d84ffdfea7d6590574b1cdfdaec695728546b2
Cobatsu <huze.ozr@gmail.com>
2024-03-29 09:49:15 -0400

    lol

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose
  .catch(err => console.log(err));
 const app = express();
 const ChatRoom = require("./Models/ChatRoomModel");
-
+//
 const getTokenPayload = token => {
  const payload = jwt.verify(token, "secret");
  return payload;
